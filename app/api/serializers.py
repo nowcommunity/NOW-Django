41d50ca49d2272ac3d3ee1eacd6f6213c3eae3b4
chzesa <chzesa+git@gmail.com>
2023-03-30 19:57:07 +0300

    Add serializers for NowLocality and the museum m2m table

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -5,3 +5,13 @@ class MuseumSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = now_models.ComMuseumList
 		exclude = ('used_morph', 'used_now', 'used_gene')
+
+class NowMuseumSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = now_models.NowMuseum
+		fields = ('__all__')
+
+class LocalitySerializer(serializers.ModelSerializer):
+	class Meta:
+		model = now_models.NowLocality
+		exclude = ()

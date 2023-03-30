6c0d5298f9aa59d2282d5cffb7406a666a98573f
chzesa <chzesa+git@gmail.com>
2023-03-30 19:37:15 +0300

    Generate and serve static files

--- app/config/settings.py
+++ app/config/settings.py
@@ -178,7 +178,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = get_var('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
 
 # Media files

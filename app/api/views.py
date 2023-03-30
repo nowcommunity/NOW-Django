abd46da3a49ce3760684a7ce470a3de12cf9b777
chzesa <chzesa+git@gmail.com>
2023-03-30 20:04:36 +0300

    Add base class for viewsets to enable setting global properties e.g. permissions

--- app/api/views.py
+++ app/api/views.py
@@ -3,12 +3,20 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.routers import APIRootView
+from rest_framework import permissions
+from django_filters.views import FilterMixin
 
 from . import serializers as api_serializers
 from now_app import models as now_models
 # Create your views here.
 
-class MuseumViewSet(viewsets.ReadOnlyModelViewSet):
+class BaseViewSet(viewsets.ModelViewSet, FilterMixin):
+	permission_classes = [permissions.DjangoModelPermissions]
+
+	class Meta:
+		abstract = True
+
+class MuseumViewSet(BaseViewSet):
 	serializer_class = api_serializers.MuseumSerializer
 	queryset = serializer_class.Meta.model.objects.all()
 
